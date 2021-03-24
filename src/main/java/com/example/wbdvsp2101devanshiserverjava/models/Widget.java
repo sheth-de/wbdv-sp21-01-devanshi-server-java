86bb1ebafdd689263ecfb5f1fe90eef92c845d3b
[sheth-de] <[sheth.de@northeastern.edu]>
2021-03-23 22:35:26 -0400

    Creating a data server

---
+++ src/main/java/com/example/wbdvsp2101devanshiserverjava/models/Widget.java
@@ -0,0 +1,139 @@
+package com.example.wbdvsp2101devanshiserverjava.models;
+
+public class Widget {
+    private Long id;
+    private String type;
+    private Integer size;
+    private String text;
+    private String topicId;
+//    added the following fields for widgets in the api also added the respective getters and setters
+    private String name;
+    private Integer widgetOrder;
+    private String srcUrl;
+    private Integer width;
+    private Integer height;
+    private String cssClass;
+    private String style;
+    private String value;
+
+    public Integer getWidgetOrder() {
+        return widgetOrder;
+    }
+
+    public void setWidgetOrder(Integer widgetOrder) {
+        this.widgetOrder = widgetOrder;
+    }
+
+    public String getSrcUrl() {
+        return srcUrl;
+    }
+
+    public void setSrcUrl(String srcUrl) {
+        this.srcUrl = srcUrl;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(String topicId) {
+        this.topicId = topicId;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Widget() {
+    }
+
+    public Widget(Long id, String topicId, String type, Integer size, String text,
+                  String name, Integer widgetOrder, String srcUrl, Integer width,
+                  Integer height, String cssClass,String style, String value
+                  ) {
+        this.id = id;
+        this.topicId = topicId;
+        this.type = type;
+        this.size = size;
+        this.text = text;
+        this.name=name;
+    }
+}
